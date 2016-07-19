30afacfc1890f8bac2412a9c770fa92baec46d83
Chris <orthagonal1@gmail.com>
2016-07-19 14:46:36 -0500

    basic version is working
    probably needs some more error testing

--- index.js
+++ index.js
@@ -1,52 +1,29 @@
 'use strict';
 const async = require('async');
-const _ = require('lodash');
 
 // first param is a function that accepts a callback who's params are an error or the list to process
-// second param is the spec for the async.auto flow to apply to each autoMethod in the list
+// second param is a function that returns a spec for the async.auto flow to apply to each autoMethod in the list
 // the third param is a callback function that combines the original listed item with the
 // result of applying async.auto to that item
-// fourth param takes in a
-module.exports = (createListFunc, autoSpecObject, autoResultFunction, reduceResultsFunction) => {
-  createListFunc((err, listToProcess) => {
+// fourth param takes in all results and processes all of them:
+module.exports = (createList, createAutoSpec, handleAutoResult, reduceResults) => {
+  createList((err, listToProcess) => {
     if (err) {
       throw err;
     }
+    const newResults = [];
     async.each(listToProcess, (currentItem, currentItemDone) => {
-      const modifiedAutoSpecObject = _.clone(autoSpecObject);
-      // wrap each function call in the autoSpecObject so that it takes in
-      // the currentItem:
-      const getWrapper = (functionToWrap) => {
-        if (functionToWrap.length === 1) {
-          return functionToWrap;
-          // don't think we need to modify here
-        } else if (functionToWrap.length === 2) {
-          return (done) => {
-            return functionToWrap(currentItem, done);
-          };
-        } else if (functionToWrap.length === 3) {
-          return (results, done) => {
-            return functionToWrap(currentItem, results, done);
-          };
+      async.auto(createAutoSpec(currentItem), (autoErr, anAutoResult) => {
+        if (autoErr) {
+          throw autoErr;
         }
-      }
-      _.each(modifiedAutoSpecObject, (autoMethod, autoDirective) => {
-        if (_.isFunction(autoMethod)) {
-          modifiedAutoSpecObject[autoDirective] = getWrapper(modifiedAutoSpecObject[autoDirective]);
-        } else if (_.isArray(autoMethod)) {
-          const functionIndex = autoMethod.length - 1;
-          const originalFunc = autoMethod[functionIndex];
-          console.log('---------------originalFunc:')
-          console.log(originalFunc)
-          console.log('========== from ')
-          console.log(autoMethod)
-          modifiedAutoSpecObject[autoDirective][functionIndex] = getWrapper(originalFunc);
-        }
+        handleAutoResult(currentItem, anAutoResult, (handleAutoResultErr, handleAutoResultOutcome) => {
+          newResults.push(handleAutoResultOutcome);
+          currentItemDone();
+        });
       });
-      async.auto(modifiedAutoSpecObject, (err, anAutoResult) => {
-        console.log(err);
-        console.log(anAutoResult);
-      });
+    }, (eachErr) => {
+      reduceResults(eachErr, newResults);
     });
   });
 };
